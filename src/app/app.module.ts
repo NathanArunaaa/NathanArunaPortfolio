31397f18b2062b2eaae90aa0c6e488508273d1cd
NathanArunaaa <nathanaruna07@gmail.com>
2023-05-09 11:23:23 -0400

    loading spinner

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,6 +9,9 @@ import { ProjectsComponent } from './projects/projects.component';
 import { ToolsComponent } from './tools/tools.component';
 import { TopnavComponent } from './topnav/topnav.component';
 import { AwardsComponent } from './awards/awards.component';
+import { SpinnerComponent } from './spinner/spinner.component';
+import { LoadingInterceptor } from './loading.interceptor';
+
 
 @NgModule({
   declarations: [
@@ -16,13 +20,19 @@ import { AwardsComponent } from './awards/awards.component';
     ProjectsComponent,
     ToolsComponent,
     TopnavComponent,
-    AwardsComponent
+    AwardsComponent,
+    SpinnerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS, useClass: LoadingInterceptor, multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
