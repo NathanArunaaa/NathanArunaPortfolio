dd5d54f2c1d1e70f5116f060358f003cdc06a63b
NathanArunaaa <nathanaruna07@gmail.com>
2023-06-16 14:23:15 -0400

    publication tab added + font colour and size fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { ProjectsComponent } from './projects/projects.component';
 import { ToolsComponent } from './tools/tools.component';
 import { AwardsComponent } from './awards/awards.component';
 import { GalleryComponent } from './gallery/gallery.component';
+import { PublicationsComponent } from './publications/publications.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
@@ -13,7 +14,8 @@ const routes: Routes = [
   {path: 'projects', component: ProjectsComponent},
   {path: 'tools', component: ToolsComponent},
   {path: 'awards', component: AwardsComponent},
-  {path: 'gallery', component: GalleryComponent}
+  {path: 'gallery', component: GalleryComponent},
+  {path: 'publications', component: PublicationsComponent}
 ];
 
 @NgModule({
